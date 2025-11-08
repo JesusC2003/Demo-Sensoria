35e71244505a15fc9df0592bdd5ce9fbc0ec2adb
Jesus Castillo <jesusecastillo@unicesar.edu.co>
2025-11-08 18:57:46 -0500

    cambios en el main

--- lib/UI/app.dart
+++ lib/UI/app.dart
@@ -1,16 +1,21 @@
-import 'package:demostracion_aplicativo/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class CapacitiveTouchDemoApp extends StatelessWidget {
-  const CapacitiveTouchDemoApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sensor Capacitivo de Contacto',
+      title: 'Material App',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const TouchScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-// lib/main.dart
 import 'package:demostracion_aplicativo/UI/app.dart';
 import 'package:flutter/material.dart';
 
-
-void main() {
-  runApp(const CapacitiveTouchDemoApp());
-}
+void main() => runApp(const MyApp());
 
